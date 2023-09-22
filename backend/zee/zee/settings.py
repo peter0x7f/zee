a89fb415a1920f478e8db849253277449ae01ac1
k1dd <50468072+peter0x7f@users.noreply.github.com>
2023-09-22 13:10:22 -0400

    changes

--- backend/zee/zee/settings.py
+++ backend/zee/zee/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@pnx*d0dy)+3)1g1p!q7j1-y3srn#eeuyo_(rw=wy(-2id88b%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["localhost"]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
