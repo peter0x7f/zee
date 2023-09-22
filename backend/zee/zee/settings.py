50d1103342006dbc3dbdd35471df9c1469d831c3
k1dd <50468072+peter0x7f@users.noreply.github.com>
2023-09-22 13:41:03 -0400

    update packages

--- backend/zee/zee/settings.py
+++ backend/zee/zee/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@pnx*d0dy)+3)1g1p!q7j1-y3srn#eeuyo_(rw=wy(-2id88b%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
--- backend/zee/zee/urls.py
+++ backend/zee/zee/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('core.urls')),
+    path('', include('core.urls')),
     path('', include(router.urls)),
 ]
 
