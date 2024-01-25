369e49083e79b62969b04dd8141ded02972f3143
josephsomogie <128629560+josephsomogie@users.noreply.github.com>
2024-01-25 15:49:42 -0500

    Docs

--- expoFrontend/zeefrontend/app/(auth)/SignOut.js
+++ expoFrontend/zeefrontend/app/(auth)/SignOut.js
@@ -1,49 +1,11 @@
-import {React, useState} from "react";
-import axios from 'axios'
-import { Asset } from 'expo-asset';
+
 import { Link, router } from 'expo-router';
-import {Image} from 'react-native'
-import { GluestackUIProvider,  Box } from "@gluestack-ui/themed";
-import { config} from "@gluestack-ui/config";
-//import { Image } from "@gluestack-ui/themed"
-import { InputField, Input, Button, ButtonText, ButtonIcon, Heading, Center } from "@gluestack-ui/themed"
-import { Divider } from "@gluestack-ui/themed";
 import * as SecureStore from 'expo-secure-store';
-
-
-import{
-  Dimensions,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TextInput,
-  useColorScheme,
-  View,
-  TouchableWithoutFeedback,
-  Keyboard
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import react from "react";
-import styles from "../stylefile";
-
-
-const screenWidth = Dimensions.get('window').width;
 /*
 Simple func signs user out and checks for errors.
 */ 
 const SignOut = async () =>
 {
-
-
   //DELETE AUTH TOKENS
     await SecureStore.deleteItemAsync('Token');
     await SecureStore.deleteItemAsync('Refresh');
@@ -55,10 +17,8 @@ const SignOut = async () =>
   //IF SUCCESSFULY DELETED
     if(Token == null && Refresh == null && username == null){
         console.log("Signout Success")
-        //S
       router.replace('/LoginP')
-
-    }
+    }//IF TOKENS STILL ACCESSIBLE
     else if(Token != null || Refresh != null || username != null)
     {
         console.log("Signout Failed! Token =", JSON.stringify(Token), " Refresh =",  JSON.stringify(Refresh))
