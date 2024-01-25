142e061933113b31520cf6bcc22237919bdf6229
k1dd <50468072+peter0x7f@users.noreply.github.com>
2024-01-25 15:57:42 -0500

    remove unwanted model import

--- backend/zee/core/serializers.py
+++ backend/zee/core/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Profile, Posts, Comment, CustomUser
+from .models import Profile, Posts, Comment
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
